aa65739c00fade92a9b3c689d1db324abe6a4984
adamgothmann <adamgothmann@gmail.com>
2016-09-03 21:00:25 -0500

    trying with jquery

--- public/scripts/fantasy.js
+++ public/scripts/fantasy.js
@@ -1,14 +1,25 @@
-var myApp = angular.module( 'myApp', [] );
-
-myApp.controller( 'apiController',[ '$scope', '$http', function( $scope, $http ){
-console.log('test');
-  var urll = 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk';
-  console.log(url);
-
-  $http({
-    method: 'GET',
-    url: urll
-  }).then(function(response){
-    console.log(response.data);
+// var myApp = angular.module( 'myApp', [] );
+//
+// myApp.controller( 'apiController',[ '$scope', '$http', function( $scope, $http ){
+// console.log('test');
+//   var urll = 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk';
+//   console.log(url);
+//
+//   $http({
+//     method: 'GET',
+//     url: urll
+//   }).then(function(response){
+//     console.log(response.data);
+//   });
+// }]);
+$(document).ready(function(){
+  var allPlayers;
+  $.ajax({
+    url: 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk',
+    type: 'GET',
+    success: function(data){
+      allPlayers = data;
+      console.log(allPlayers);
+    }
   });
-}]);
+});
