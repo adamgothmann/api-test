0b0ac0b8ffbc3adc4530935ef0ec4f368bcf3386
adamgothmann <adamgothmann@gmail.com>
2016-09-05 23:22:02 -0500

    working on ajax

--- public/scripts/fantasy.js
+++ public/scripts/fantasy.js
@@ -1,6 +1,6 @@
-// var myApp = angular.module( 'myApp', [] );
-//
-// myApp.controller( 'apiController',[ '$scope', '$http', function( $scope, $http ){
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'apiController',[ '$scope', '$http', function( $scope, $http ){
 // console.log('test');
 //   var urll = 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk';
 //   console.log(url);
@@ -11,15 +11,28 @@
 //   }).then(function(response){
 //     console.log(response.data);
 //   });
-// }]);
-$(document).ready(function(){
-  var allPlayers;
-  $.ajax({
-    url: 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk',
-    type: 'GET',
-    success: function(data){
-      allPlayers = data;
-      console.log(allPlayers);
-    }
-  });
+console.log('something wut');
+$scope.something = function(){
+  console.log('something');
+$.ajax({
+  url: 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk',
+  type: 'GET',
+  success: function(data){
+    allPlayers = data;
+    console.log(allPlayers);
+  }
 });
+};
+$scope.something();
+}]);
+// $(document).ready(function(){
+//   var allPlayers;
+//   $.ajax({
+//     url: 'http://www.fantasyfootballnerd.com/service/players/json/j7rfhwpep6dk',
+//     type: 'GET',
+//     success: function(data){
+//       allPlayers = data;
+//       console.log(allPlayers);
+//     }
+//   });
+// });
